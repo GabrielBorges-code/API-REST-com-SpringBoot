15f43c9e1d8ad900f4f26ae57682d485c29cd10e
Gabriel B <gabriel.moura2009@hotmail.com>
2020-09-22 17:23:24 -0300

    request https okay

--- src/main/java/com/example/carros/api/IndexController.java
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -2,6 +2,8 @@ package com.example.carros.api;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 @RequestMapping("/")
 public class IndexController {
@@ -12,23 +14,19 @@ public class IndexController {
         return "Hello Spring Boot";
     }
 
-    @PostMapping()
-    //@RequestMapping(method = RequestMethod.GET) isso é a mesma coisa do get mapping
-    public String post(){
-        return "Post Spring Boot";
+    @PostMapping("/login")
+    public String login(@RequestParam ("login") String login, @RequestParam("senha") String senha){
+        return "Login: " + login + ", Senha: " + senha;
+    }
+/*
+    @GetMapping("/carros/{id}")
+    public String getCarroById(@PathVariable("id") Long id){
+        return "Carro by id: " + id;
     }
 
-    @PutMapping()
-    //@RequestMapping(method = RequestMethod.GET) isso é a mesma coisa do get mapping
-    public String put(){
-        return "Put Spring Boot";
-    }
-
-    @DeleteMapping()
-    //@RequestMapping(method = RequestMethod.GET) isso é a mesma coisa do get mapping
-    public String delete(){
-        return "Delete Spring Boot";
-    }
-
+    @GetMapping("/carros/tipo/{tipo}")
+    public String getCarroByTipo(@PathVariable("tipo") String tipo){
+        return "Lista de tipo de carros " + tipo;
+    }*/
 
 }
