bf4ad8510eed60321cb327834f96955f736819e1
Gabriel B <gabriel.moura2009@hotmail.com>
2020-09-29 19:20:40 -0300

    database and injection dependency

--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -1,13 +1,25 @@
 package com.example.carros.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarroService {
 
-    public List<Carro> getCarros() {
+    @Autowired
+    private CarroRepository rep;
+
+    public Iterable<Carro> getCarros() {
+        return rep.findAll();
+    }
+
+    public List<Carro> getCarrosFake() {
         List<Carro> carros = new ArrayList<>();
 
+        //Array im memory
         carros.add(new Carro(1L,"Fusca"));
         carros.add(new Carro(2L,"Brasilia"));
         carros.add(new Carro(3L,"Chevette"));
