399c9465c6050cbd4789f7e3da04415f1145ec0c
Gabriel B <gabriel.moura2009@hotmail.com>
2020-09-30 23:26:36 -0300

    put with update

--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -2,6 +2,7 @@ package com.example.carros.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,9 +41,39 @@ public class CarroService {
         return carros;
     }
 
-    public Carro save(Carro carro) {
+    /*public Carro save(Carro carro) {
 
         return rep.save(carro);
+
+    }*/
+
+    public Carro insert(Carro carro) {
+
+        Assert.isNull(carro.getId(), "Não foi possível inserir o registro");
+
+        return rep.save(carro);
+    }
+
+    public Carro update(Carro carro, Long id) {
+
+        Assert.notNull(id,"Não foi possível alterar o registro");
+
+        //Busca o dado no banco de dados
+        Optional<Carro> optional = getCarrosById(id);
+        if(optional.isPresent()) {
+            Carro db = optional.get();
+            //Copia as propriedades
+            db.setNome(carro.getNome());
+            db.setTipo(carro.getTipo());
+            System.out.println("Carro id " + db.getId());
+
+            //Atualiza carro
+            rep.save(db);
+
+            return db;
+        }else{
+            throw new RuntimeException("Não foi possivel atualizar o registro");
+        }
 
     }
 }
